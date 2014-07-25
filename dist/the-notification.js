88b09b1923f9dc30832e0ea1c662108c152a5b46
Thiago Lagden <lagden@gmail.com>
2014-07-25 19:29:40 -0300

    Depends gsap

--- dist/the-notification.js
+++ dist/the-notification.js
@@ -6,73 +6,68 @@ It is a plugin that show notification like Growl
 
 @author      Thiago Lagden <lagden [at] gmail.com>
 @copyright   Author
+
+Depends:
+TweenMax.js
  */
+var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
+
 (function(root, factory) {
   if (typeof define === "function" && define.amd) {
-    define(['getStyleProperty'], factory);
+    define(['TweenMax'], factory);
   } else {
-    root.TheNotification = factory(root.getStyleProperty);
+    root.TheNotification = factory(root.TweenMax);
   }
-})(this, function(getStyleProperty) {
+})(this, function(TM) {
   'use strict';
-  var TheNotification, animate, extend, getTemplate, handlerRemove, options, transformProp;
-  getTemplate = function() {
-    return ['<h3 class="theNotification__title">{title}</h3>', '<p class="theNotification__msg">{msg}</p>'].join('');
-  };
-  extend = function() {
-    var args, key, out, _i, _len;
-    out = out || {};
-    for (_i = 0, _len = arguments.length; _i < _len; _i++) {
-      args = arguments[_i];
-      if (args === false) {
-        continue;
-      }
-      for (key in args) {
-        if (args.hasOwnProperty(key)) {
-          out[key] = args[key];
+  var TheNotification, eventType, hasPointerEvents, hasTouchEvents, isTouch, options, _privados;
+  hasPointerEvents = Boolean(window.navigator.pointerEnabled || window.navigator.msPointerEnabled);
+  hasTouchEvents = __indexOf.call(window, 'ontouchstart') >= 0;
+  isTouch = Boolean(hasTouchEvents || hasPointerEvents);
+  eventType = isTouch ? 'touchend' : 'click';
+  _privados = {
+    getTemplate: function() {
+      return ['<h3 class="theNotification__title">{title}</h3>', '<p class="theNotification__msg">{msg}</p>'].join('');
+    },
+    extend: function() {
+      var args, key, out, _i, _len;
+      out = out || {};
+      for (_i = 0, _len = arguments.length; _i < _len; _i++) {
+        args = arguments[_i];
+        if (args === false) {
+          continue;
+        }
+        for (key in args) {
+          if (args.hasOwnProperty(key)) {
+            out[key] = args[key];
+          }
         }
       }
+      return out;
+    },
+    remove: function(item, ev) {
+      this.remove(item);
+    },
+    selfRemove: function(item) {
+      setTimeout((function() {
+        return this.remove(item);
+      }).bind(this), this.opts.duration);
     }
-    return out;
-  };
-  animate = function(opts) {
-    var run, start;
-    start = new Date().getTime();
-    run = function() {
-      var delta, progress, timePassed;
-      timePassed = new Date().getTime() - start;
-      progress = timePassed / opts.duration;
-      if (progress > 1) {
-        progress = 1;
-      }
-      delta = opts.delta(progress);
-      opts.step(delta);
-      if (progress === 1) {
-        opts.complete();
-      } else {
-        requestAnimationFrame(run);
-      }
-    };
-    requestAnimationFrame(run);
-  };
-  handlerRemove = function(item, ev) {
-    this.remove(item);
   };
   options = {
     duration: 5000,
     container: null,
     offset: 10
   };
-  transformProp = getStyleProperty('transform');
   TheNotification = (function() {
     function TheNotification(opts) {
       if (false === (this instanceof TheNotification)) {
         return new TheNotification(opts);
       }
-      this.opts = extend({}, options, opts);
+      this.opts = _privados.extend({}, options, opts);
       this.items = [];
       this.container = this.opts.container || document.body;
-      this.template = this.opts.template || getTemplate();
+      this.template = this.opts.template || _privados.getTemplate();
       return;
     }
 
@@ -96,7 +91,7 @@ It is a plugin that show notification like Growl
         offset[1] = parseInt(offset[0] + last.offsetHeight + this.opts.offset, 10);
       }
       item.setAttribute('data-offset', offset[1]);
-      item.addEventListener('click', handlerRemove.bind(this, item), false);
+      item.addEventListener(eventType, _privados.remove.bind(this, item), false);
       this.items.push(item);
       this.render(item, offset);
     };
@@ -104,55 +99,33 @@ It is a plugin that show notification like Growl
     TheNotification.prototype.render = function(item, offset) {
       var from, to;
       this.container.appendChild(item);
-      item.style[transformProp] = 'translate3d(0,' + offset[0] + 'px,0)';
-      item.style.opacity = 0;
-      from = offset[0];
-      to = offset[1];
-      animate({
-        duration: 500,
-        delta: function(p) {
-          return 1 - Math.sin(Math.acos(p));
-        },
-        step: function(d) {
-          var t;
-          t = parseInt((d * (to - from)) + from, 10);
-          item.style[transformProp] = 'translate3d(0,' + t + 'px,0)';
-          item.style.opacity = d;
-        },
-        complete: (function() {
-          setTimeout((function() {
-            this.remove(item);
-          }).bind(this), this.opts.duration);
-        }).bind(this)
-      });
+      from = {
+        y: offset[0],
+        opacity: 0
+      };
+      to = {
+        y: offset[1],
+        opacity: 1,
+        onComplete: _privados.selfRemove.bind(this, item)
+      };
+      TM.fromTo(item, 0.5, from, to);
     };
 
     TheNotification.prototype.remove = function(item) {
-      var from, index, to;
+      var index, to;
+      item.removeEventListener(eventType, _privados.remove);
       index = this.items.indexOf(item);
-      item.removeEventListener('click', handlerRemove);
-      if (index === -1) {
-        return this;
+      if (index !== -1) {
+        this.items.splice(index, 1);
       }
-      this.items.splice(index, 1);
-      from = parseInt(item.style[transformProp].replace(/translate3d\(0px, ([\-0-9]+)px, 0px\)/gi, '$1'), 10);
-      to = from - 30;
-      animate({
-        duration: 300,
-        delta: function(p) {
-          return 1 - Math.sin(Math.acos(p));
-        },
-        step: function(d) {
-          var o, t;
-          t = parseInt((d * (to - from)) + from, 10);
-          o = (d * (0 - 1)) + 1;
-          item.style[transformProp] = 'translate3d(0,' + t + 'px,0)';
-          item.style.opacity = o;
-        },
-        complete: (function() {
-          this.container.removeChild(item);
-        }).bind(this)
-      });
+      to = {
+        y: '-=30px',
+        opacity: 0,
+        onComplete: function() {
+          return item.remove();
+        }
+      };
+      TM.to(item, 0.3, to);
     };
 
     return TheNotification;
